941985540b33462c36c896df9d6b1b38ed34be02
Avanish Patel <apatel@solstice.com>
2018-09-15 14:21:28 -0500

    first git push

---
+++ src/main/java/com/solstice/shipmentservice/repository/ShipmentRepository.java
@@ -0,0 +1,13 @@
+package com.solstice.shipmentservice.repository;
+
+import com.solstice.shipmentservice.domain.Shipment;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ShipmentRepository extends CrudRepository<Shipment, Long> {
+
+    Iterable<Shipment> findAllByAccountIdOrderByDeliveryDate(long id);
+
+    Shipment findByAccountId(long id);
+}
