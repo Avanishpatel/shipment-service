34cf18f065f9ba2bcfaf3ad9d57a0c619193c13e
Avanish Patel <apatel@solstice.com>
2018-09-20 10:04:28 -0500

    added documentation with swagger2

--- src/test/java/com/solstice/shipmentservice/controller/ShipmentControllerUnitTest.java
+++ src/test/java/com/solstice/shipmentservice/controller/ShipmentControllerUnitTest.java
@@ -1,6 +1,7 @@
 package com.solstice.shipmentservice.controller;
 
 import com.solstice.shipmentservice.domain.Shipment;
+import com.solstice.shipmentservice.domain.ShipmentAggregation;
 import com.solstice.shipmentservice.service.ShipmentService;
 import org.junit.Before;
 import org.junit.Test;
@@ -10,6 +11,7 @@ import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -19,8 +21,9 @@ import java.util.List;
 
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.*;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -58,29 +61,107 @@ public class ShipmentControllerUnitTest {
         mockMvc.perform(get("/shipments"))
                 .andExpect(jsonPath("$.length()").value(2))
                 .andExpect(jsonPath("$[0].orderLineId", is(1)))
-                .andExpect(jsonPath("$[1].shippingDate", is(LocalDate.of(2018,6,26))))
+                .andExpect(jsonPath("$[1].shippingDate", is("2018-06-26")))
                 .andExpect(content().contentType("application/json;charset=UTF-8"))
                 .andExpect(status().isOk())
                 .andReturn();
     }
 
     @Test
-    public void getShipment() {
+    public void getShipment() throws Exception {
+
+        Shipment shipment = new Shipment(2, 2, 2, LocalDate.of(2018, 6, 26), LocalDate.of(2018, 6, 28));
+
+        when(shipmentService.getShipment(any(Long.class))).thenReturn(shipment);
+
+        mockMvc.perform(get("/shipments/{id}", 2))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
+                .andExpect(jsonPath("$.shippingAddressId", is(2)))
+                .andExpect(jsonPath("$.deliveryDate", is("2018-06-28")))
+                .andReturn();
     }
 
     @Test
-    public void addShipment() {
+    public void addShipment() throws Exception {
+
+        //language=JSON
+        String json = "{\n" +
+                "  \"accountId\":2,\n" +
+                "  \"shippingAddressId\":2,\n" +
+                "  \"orderLineId\":2,\n" +
+                "  \"shippingDate\":\"2018-06-22\",\n" +
+                "  \"deliveryDate\":\"2018-06-24\"\n" +
+                "}";
+
+        Shipment shipment = new Shipment(2, 2, 2, LocalDate.of(2018, 6, 22), LocalDate.of(2018, 6, 24));
+
+        when(shipmentService.addShipment(any(Shipment.class))).thenReturn(shipment);
+
+        mockMvc.perform(post("/shipments")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(json))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.deliveryDate", is("2018-06-24")))
+                .andReturn();
     }
 
     @Test
-    public void updateShipment() {
+    public void updateShipment() throws Exception {
+
+        String json = "{\n" +
+                "  \"accountId\":2,\n" +
+                "  \"shippingAddressId\":2,\n" +
+                "  \"orderLineId\":2,\n" +
+                "  \"shippingDate\":\"2018-06-22\",\n" +
+                "  \"deliveryDate\":\"2018-06-24\"\n" +
+                "}";
+
+        Shipment shipment = new Shipment(2, 2, 2, LocalDate.of(2018, 6, 22), LocalDate.of(2018, 6, 24));
+
+        when(shipmentService.updateShipment(any(Long.class),any(Shipment.class))).thenReturn(shipment);
+
+        mockMvc.perform(put("/shipments/{id}", 2).content(json).contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.shippingDate",is("2018-06-22")))
+                .andReturn();
+
     }
 
     @Test
-    public void deleteShipment() {
+    public void deleteShipment() throws Exception {
+
+        String json = "{\n" +
+                "  \"accountId\":2,\n" +
+                "  \"shippingAddressId\":2,\n" +
+                "  \"orderLineId\":2,\n" +
+                "  \"shippingDate\":\"2018-06-22\",\n" +
+                "  \"deliveryDate\":\"2018-06-24\"\n" +
+                "}";
+
+        Shipment shipment = new Shipment(2, 2, 2, LocalDate.of(2018, 6, 22), LocalDate.of(2018, 6, 24));
+
+        when(shipmentService.deleteShipment(any(Long.class))).thenReturn(shipment);
+
+        mockMvc.perform(delete("/shipments/{id}", 2))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.accountId", is(2)));
+
     }
 
     @Test
-    public void getShipmentsByAccount() {
+    public void getShipmentsByAccount() throws Exception {
+
+        List<ShipmentAggregation> shipmentAggregations = Arrays.asList(
+                new ShipmentAggregation(1, LocalDate.of(2018, 06, 25), LocalDate.of(2018, 06, 27),"Iphone Xs", 4)
+        );
+
+        when(shipmentService.getShipmentsByAccount(any(Long.class))).thenReturn(shipmentAggregations);
+
+        mockMvc.perform(get("/shipments/account/{id}",1))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].productName",is("Iphone Xs")))
+                .andReturn();
+
     }
 }
